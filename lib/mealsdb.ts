899a52ceede548e49c895c406bc63937da509929
Paul <Paul---@users.noreply.github.com>
2026-02-11 17:25:45 -0600

    add meals details page

--- lib/mealsdb.ts
+++ lib/mealsdb.ts
@@ -2,9 +2,47 @@ import sql from 'better-sqlite3';
 
 const db = sql('meals.db');
 
+interface Meal {
+  title: string;
+  slug: string;
+  image: string;
+  summary: string;
+  instructions: string;
+  creator: string;
+  creator_email: string;
+}
+
 // async to simulate fetching data from a remote database
 export async function getMeals() {
   // eslint-disable-next-line no-undef
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return db.prepare('SELECT * FROM meals').all();
+}
+
+function isMeal(value: unknown): value is Meal {
+  if (!value || typeof value !== 'object') return false;
+
+  return (
+    'title' in value &&
+    typeof value.title === 'string' &&
+    'slug' in value &&
+    typeof value.slug === 'string' &&
+    'image' in value &&
+    typeof value.image === 'string' &&
+    'summary' in value &&
+    typeof value.summary === 'string' &&
+    'instructions' in value &&
+    typeof value.instructions === 'string' &&
+    'creator' in value &&
+    typeof value.creator === 'string' &&
+    'creator_email' in value &&
+    typeof value.creator_email === 'string'
+  );
+}
+
+export async function getMeal(slug: string) {
+  // eslint-disable-next-line no-undef
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const result = db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+  return isMeal(result) ? (result as Meal) : null;
 }
